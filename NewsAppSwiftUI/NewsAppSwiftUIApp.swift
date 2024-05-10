b09e1c887b65bace03fd5214be325d3af192cec0
Şule <45969586+sulekaptan@users.noreply.github.com>
2024-05-10 22:48:06 +0300

    Initial Commit

---
+++ NewsAppSwiftUI/NewsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppSwiftUIApp.swift
+//  NewsAppSwiftUI
+//
+//  Created by Şule Kaptan on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
